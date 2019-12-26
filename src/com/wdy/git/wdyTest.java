7d9a9423b54ca7ac808607fe9d3a2d2474e7036b
wdy <251612073@qq.com>
2019-12-26 09:46:57 +0800

    My First add

---
+++ src/com/wdy/git/wdyTest.java
@@ -0,0 +1,8 @@
+package com.wdy.git;
+
+public class wdyTest {
+    public static void main(String[] args) {
+        System.out.println("21351764tregybfr");
+        System.out.println("hf873420fnsfepwji3");
+    }
+}
